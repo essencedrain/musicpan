0b0af94827daea873c8860007825e2d5c6534506
essencedrain <moonkero@naver.com>
2020-05-25 16:56:03 +0900

    기능 : 회원가입 닉네임, 이메일 ajax 유효성 추가

--- musicpan/src/main/java/com/musicpan/mapper/MemberMapper.java
+++ musicpan/src/main/java/com/musicpan/mapper/MemberMapper.java
@@ -7,4 +7,7 @@ public interface MemberMapper {
 	public MemberVO read(String id);
 	public int insert(MemberVO vo);
 	public int insert_auth1(String id);
+	public int checkId(String id);
+	public int checkName(String name);
+	public int checkEmail(String email);
 }
--- musicpan/src/main/java/com/musicpan/service/MemberService.java
+++ musicpan/src/main/java/com/musicpan/service/MemberService.java
@@ -5,6 +5,8 @@ import com.musicpan.domain.MemberVO;
 public interface MemberService {
 
 	public boolean member_validation(String id);
+	public boolean member_Name_validation(String name);
+	public boolean member_Email_validation(String email);
 	public boolean register(MemberVO vo);
 	
 }//class
